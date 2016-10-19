adfd38a21e02e6e806e5adde994240e3d6d3ad85
Bartosz Kamiński <bartosz.kaminski@zoho.com>
2016-10-19 14:00:27 -0400

    wildfly + postgres - work in progres

--- ci/postgres-dev/run.sh
+++ ci/postgres-dev/run.sh
@@ -1,1 +1,1 @@
-docker run --name links/postgres -e POSTGRES_PASSWORD=postgressecretpassword -d links/postgres
+docker run --name postgres-dev -p 5432:5432 -e POSTGRES_PASSWORD=postgressecretpassword links/postgres-dev
