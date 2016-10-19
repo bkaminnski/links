0d1e63a7560cadfcb2c5d8cd047fd8abe0af3516
Bartosz Kamiński <bartosz.kaminski@zoho.com>
2016-10-20 00:45:34 +0200

    links are now persisted in db

--- sources/Links/src/main/java/com/hclc/Link.java
+++ sources/Links/src/main/java/com/hclc/Link.java
@@ -1,11 +1,32 @@
 package com.hclc;
 
+import java.io.Serializable;
 import javax.json.Json;
 import javax.json.JsonObjectBuilder;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
-public class Link {
+@Entity
+public class Link implements Serializable {
 
-    private final String url, keywords, description;
+    @Id
+    @SequenceGenerator(name = "link_id_seq",
+            sequenceName = "link_id_seq",
+            allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+            generator = "link_id_seq")
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(length = 2000)
+    private String url, keywords, description;
+
+    public Link() {
+    }
 
     public Link(String url, String keywords, String description) {
         this.url = url;
--- sources/Links/src/main/java/com/hclc/Links.java
+++ sources/Links/src/main/java/com/hclc/Links.java
@@ -1,10 +1,12 @@
 package com.hclc;
 
-import java.util.stream.Stream;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,14 +16,15 @@ import javax.ws.rs.core.MediaType;
 @Path(value = "links")
 public class Links {
 
+    @PersistenceContext
+    EntityManager em;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public JsonArray links() {
-        return Stream.of(
-                new Link("https://www.youtube.com/watch?v=A800BaLBB2k", "Adam Bien, react, JEE", "Java EE becomes an interesting platform for exposing services for mobile apps. To give you a feeling about the productivity, I installed a CORS filter, implemented, built and deployed a Java EE 7 service from scratch, exposed a JSON-array, implemented a HTTP client using stock XMLHttpRequest and rendered the result using the React JavaScript library."),
-                new Link("https://www.youtube.com/watch?v=Vt4G-pHXfs4", "Christopher Batey, docker, JVM, Devoxx Poland 2016", "Containers are the latest hype. It goes without saying that Docker for the development environment is a good thing but what about running our production Java applications inside a container?"),
-                new Link("https://www.youtube.com/watch?v=w5TupxbnnrM", "Chris Hawkes, react, babel, webpack", "Things change in the JavaScript world so fast nowadays. I feel this video is relatively future proof going into 2017 as to how babel, react and webpack should be used together to make development easier.")
-        )
+        return ((List<Link>) em.createQuery("select l from Link l")
+                .getResultList())
+                .stream()
                 .map(Link::toJson)
                 .collect(Json::createArrayBuilder, JsonArrayBuilder::add, JsonArrayBuilder::add)
                 .build();
