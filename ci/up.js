ca1ed5e865d447498ea4f8b1fe2a862b1c569214
Bartosz Kamiński <bartosz.kaminski@zoho.com>
2016-10-21 18:04:13 -0400

    up.sh script to start all containers from scratch - work in progress

--- ci/up.js
+++ ci/up.js
@@ -1,13 +1,14 @@
 #!/usr/bin/jjs -fv
 
 var dockerImages = new DockerImages();
-dockerImages.buildImage('java', 'links/java');
-dockerImages.buildImage('wildfly', 'links/wildfly');
-dockerImages.buildImage('wildfly', 'links/wildfly-dev');
-dockerImages.buildImage('postgres-dev', 'links/postgres-dev');
+dockerImages.build('java', 'links/java');
+dockerImages.build('wildfly', 'links/wildfly');
+dockerImages.build('wildfly', 'links/wildfly-dev');
+dockerImages.build('postgres-dev', 'links/postgres-dev');
 
-// $EXEC('docker run --name=wildfly-dev -d -p 8080:8080 -p 9990:9990 --link=postgres-dev links/wildfly-dev');
-// print($OUT);
+var dockerContainers = new DockerContainers();
+dockerContainers.run('postgres-dev', 'links/postgres-dev', '-p 5432:5432 -e POSTGRES_PASSWORD=postgressecretpassword');
+dockerContainers.run('wildfly-dev', 'links/wildfly-dev', '-p 8080:8080 -p 9990:9990 --link=postgres-dev');
 
 function DockerImages() {
         this.existingImages = loadExistingImages();
@@ -17,16 +18,60 @@ function DockerImages() {
                 return $OUT;
         }
 
-        this.buildImage = function(folder, imageName) {
-                if (this.existingImages.indexOf(imageName) < 0) {
-                        print('building ' + imageName + ' image...');
-                        $ENV.PWD = './' + folder;
-                        $EXEC('docker build -t ' + imageName + ' .');
-                        print($OUT)
-                } else {
-                        print(imageName + ' image already exists');
+        this.build = function(folder, image) {
+                if (this.exists(image)) {
+                        print(image + ' image already exists');
+			return;
                 }
+                print('building ' + image + ' image...');
+                $ENV.PWD = './' + folder;
+                $EXEC('docker build -t ' + image + ' .');
+                print($OUT)
         }
+
+	this.exists = function(image) {
+		return this.existingImages.indexOf(image) >= 0;
+	}
 }
 
+function DockerContainers() {
+	this.runningContainers = loadRunningContainers();
+	this.existingContainers = loadExistingContainers();
+	
+	function loadRunningContainers() {
+		$EXEC('docker ps');;
+                return $OUT;
+	}
+
+	function loadExistingContainers() {
+		$EXEC('docker ps -a');
+                return $OUT;
+	}
+
+	this.run = function(container, image, parameters) {
+		if (this.isRunning(container)) {
+			print(container + ' container is already running');
+			return;
+		}
+
+		if (!this.exists(container)) {
+			print('running ' + container + ' container...');
+			$EXEC('docker run --name ' + container + ' -d ' + parameters + ' ' + image);
+			print($OUT);
+			return;
+		}
+
+		print('starting ' + container + ' container...');
+		$EXEC('docker start ' + container);
+		print($OUT);
+	}
+
+	this.isRunning = function(container) {
+		return this.runningContainers.indexOf(container) >= 0;
+	}
+
+	this.exists = function(container) {
+		return this.existingContainers.indexOf(container) >= 0;
+	}
+}
 
--- ci/wildfly-dev/run.sh
+++
@@ -1,1 +0,0 @@
-docker run --name=wildfly-dev -d -p 8080:8080 -p 9990:9990 --link=postgres-dev links/wildfly-dev
