6a947a295894ae9c6c28d60311fc311fd1842fa5
Bartosz Kamiński <bartosz.kaminski@powel.no>
2017-11-12 17:13:56 +0100

    after db is reloaded, wildfly needs to be restarted

--- ci/reloadDb.js
+++ ci/reloadDb.js
@@ -3,10 +3,13 @@
 var timeStarted = new Date().getTime();
 
 load('./scripts/databaseUp.js');
+load('./scripts/middlewareUp.js');
 
 new Command('.', 'docker rm -f postgres-configured').execute();
 new Command('.', 'docker rmi postgres-configured').execute();
+new Command('.', 'docker kill wildfly-configured').execute();
 
 databaseUp();
+middlewareUp();
 
 print('Script finished after ' + (new Date().getTime() - timeStarted) + ' millis');
